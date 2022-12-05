0560c195e8c1a716f22ce8bee6d151f719e71130
Chris C.H. Lin <chunhung.lin@citrix.com>
2022-12-05 21:36:28 +0000

    End of ch5.3

--- src/main/java/com/chrischlin/spring/learn/config/ApplicationConfig.java
+++ src/main/java/com/chrischlin/spring/learn/config/ApplicationConfig.java
@@ -3,6 +3,7 @@ package com.chrischlin.spring.learn.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.PropertySource;
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "com.chrischlin.spring.learn")
+@EnableAspectJAutoProxy
 public class ApplicationConfig {
 
 
--- src/main/java/com/chrischlin/spring/learn/service/GreetingService.java
+++ src/main/java/com/chrischlin/spring/learn/service/GreetingService.java
@@ -1,5 +1,6 @@
 package com.chrischlin.spring.learn.service;
 
+import com.chrischlin.spring.learn.aspect.Loggable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ public class GreetingService {
         super();
     }
 
+    @Loggable
     public String getGreeting(String name){
         return greeting + " " + name;
     }
