afe427948d528626e3105b620197844a079360a2
Chris C.H. Lin <chunhung.lin@citrix.com>
2022-12-05 17:25:06 +0000

    End of chapter two

--- src/main/java/com/chrischlin/spring/learn/config/ApplicationConfig.java
+++ src/main/java/com/chrischlin/spring/learn/config/ApplicationConfig.java
@@ -1,12 +1,7 @@
 package com.chrischlin.spring.learn.config;
 
 
-import com.chrischlin.spring.learn.service.GreetingService;
-import com.chrischlin.spring.learn.service.OutputService;
-import com.chrischlin.spring.learn.service.TimeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
@@ -16,32 +11,8 @@ import org.springframework.context.annotation.PropertySource;
  */
 @Configuration
 @PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = "com.chrischlin.spring.learn")
 public class ApplicationConfig {
 
-    @Value("${app.greeting}")
-    private String greeting;
 
-    @Value("${app.name}")
-    private String name;
-
-    @Autowired
-    private GreetingService greetingService;
-
-    @Autowired
-    private TimeService timeService;
-
-    @Bean
-    public TimeService timeService(){
-        return new TimeService(true);
-    }
-
-    @Bean
-    public OutputService outputService(){
-        return new OutputService(greetingService, timeService, name);
-    }
-
-    @Bean
-    public GreetingService greetingService() {
-        return new GreetingService(greeting);
-    }
 }
