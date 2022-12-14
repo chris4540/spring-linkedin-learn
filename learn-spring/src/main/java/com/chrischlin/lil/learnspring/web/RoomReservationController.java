a0ca332fa59fe6fbb978051a7f085feebc291023
Chris C.H. Lin <chris4540@gmail.com>
2022-12-14 23:21:08 +0000

    Update at the end of chapter 4.3

---
+++ learn-spring/src/main/java/com/chrischlin/lil/learnspring/web/RoomReservationController.java
@@ -0,0 +1,32 @@
+package com.chrischlin.lil.learnspring.web;
+
+import com.chrischlin.lil.learnspring.business.ReservationService;
+import com.chrischlin.lil.learnspring.utils.DateUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/reservations")
+public class RoomReservationController {
+
+    private final DateUtils dateUtils;
+    private final ReservationService reservationService;
+
+    public RoomReservationController(DateUtils dateUtils, ReservationService reservationService) {
+        this.dateUtils = dateUtils;
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping
+    public String getReservation(@RequestParam(value="date", required = false) String dateString,
+                                 Model model) {
+        // parse the date
+        var date = dateUtils.createDateFromDateString(dateString);
+        var roomReservations = reservationService.getRoomReservationsForDate(date);
+        model.addAttribute("roomReservations", roomReservations);
+        return "roomres";
+    }
+}
